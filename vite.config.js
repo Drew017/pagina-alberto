1f63a91e4706ff7121648f14566e127c23ec797c
AndresMartos <98174192+AndresMartos@users.noreply.github.com>
2024-09-17 22:03:49 +0200

    archivos actualizados

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pagina-alberto/',
+  base: '/pagina-alberto',
   plugins: [react()],
 })
